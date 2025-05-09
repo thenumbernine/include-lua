20dc784d2ce225afa080fac141321191a93aaabc
Christopher Moore <christopher.e.moore@gmail.com>
2025-05-09 20:35:39 +0800

    more

--- make.lua
+++ make.lua
@@ -1,23 +1,18 @@
 #!/usr/bin/env luajit
 local ffi = require 'ffi'
 local path = require 'ext.path'
-local table = require 'ext.table'
 local os = require 'ext.os'
 local includeList = require 'include-list'
 
-local req = ...
+local req = assert(..., "`make.lua all` for all, or `make.lua <sourceIncludeFile.h>`")
 
-if not req then error("`make.lua all` for all, or `make.lua <sourceIncludeFile.h>`") end
 if req ~= 'all' then
 	-- TODO seems using <> or "" now is essential for excluding recursive require's
 	if req:sub(1,1) ~= '<' and req:sub(1,1) ~= '"' then
 		error('must be system (<...>) or user ("...") include space')
 	end
 	print('searching for '..req)
-	includeList = table.filter(includeList, function(inc)
-		--return inc.inc:match(req)
-		return inc.inc == req
-	end):setmetatable(nil)
+	includeList = includeList:filter(function(inc) return inc.inc == req end)
 	if #includeList == 0 then
 		error("couldn't find "..req)
 	end
@@ -52,7 +47,7 @@ for _,inc in ipairs(includeList) do
 		assert(os.exec([[luajit -e "assert(loadfile(']]..outpath..[['))()"]]))
 		--]=]
 		-- [=[ use require, and base it in the output folder
-		assert(os.exec([[luajit -e "package.path=']]..outdirbase..[[/?.lua;'..package.path require 'ffi.req' ']]..assert((inc.out:match('(.*)%.lua'))):gsub('/', '.')..[['"]]))
+		assert(os.exec([[luajit -e "package.path=']]..outdirbase..[[/?.lua;'..package.path require 'ffi.req' ']]..assert((inc.out:match'(.*)%.lua')):gsub('/', '.')..[['"]]))
 		--]=]
 	end
 end
