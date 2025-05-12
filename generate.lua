93457d70b8bb8535b0e13656d5743f5395b2a25a
Christopher Moore <christopher.e.moore@gmail.com>
2025-05-12 15:07:51 +0800

    noice

--- generate.lua
+++ generate.lua
@@ -946,6 +946,9 @@ os.exit(1)
 									reportDups(newIncInfo, prevIncInfo, includePath)
 								else
 									lines:insert(i+1, "]] require 'ffi.req' '"
+										-- TODO TODO TODO TODO TODO this shouldn't be including the ffi.os in it! That's the whole point of ffi.req!
+										-- but at the end of *only* the os-specific include-lists I am adding in the `OS/` prefix to the path ... because thats where the binding files get written ...
+										-- so *HERE* should I be removing it again?
 										..previnc.out:match'^(.*)%.lua$':gsub('/', '.')
 										.."' ffi.cdef[["
 --..'\n...from: '..tolua(prevIncInfo)
